4dc2182e21cfa62eda6dc04132a5986aa475413b
AVVKavvk <kumawatvipin066@gmail.com>
2025-10-05 15:43:25 +0530

    footer timer

--- src/components/countDownForFooter.tsx
+++ src/components/countDownForFooter.tsx
@@ -48,37 +48,45 @@ const CountdownTimerForFooter: React.FC<CountdownTimerForFooterProps> = ({
 
   return (
     <div className="bg-gradient-ocean p-1 rounded-xl shadow-strong text-center">
-      <div className="grid grid-cols-4 gap-4">
+      <div className="hidden md:grid grid-cols-4 gap-4">
         <div className="bg-background/10 p-1 rounded-lg">
           <div className="md:text-2xl font-bold text-primary-foreground">
             {timeLeft.days}
           </div>
-          <div className="md:block hidden text-sm text-primary-foreground/80">
-            Days
-          </div>
+          <div className=" text-sm text-primary-foreground/80">Days</div>
         </div>
         <div className="bg-background/10 p-1 rounded-lg">
           <div className="md:text-2xl font-bold text-primary-foreground">
             {timeLeft.hours}
           </div>
-          <div className=" md:block hidden text-sm text-primary-foreground/80">
-            Hours
-          </div>
+          <div className="  text-sm text-primary-foreground/80">Hours</div>
         </div>
         <div className="bg-background/10 p-1 rounded-lg">
           <div className="md:text-2xl font-bold text-primary-foreground">
             {timeLeft.minutes}
           </div>
-          <div className=" md:block hidden text-sm text-primary-foreground/80">
-            Minutes
-          </div>
+          <div className=" text-sm text-primary-foreground/80">Minutes</div>
         </div>
         <div className="bg-background/10 p-1 rounded-lg">
           <div className="md:text-2xl font-bold text-primary-foreground">
             {timeLeft.seconds}
           </div>
-          <div className="md:block hidden text-sm text-primary-foreground/80">
-            Seconds
+          <div className=" text-sm text-primary-foreground/80">Seconds</div>
+        </div>
+      </div>
+      <div className="md:hidden grid gap-4">
+        <div className="bg-background/10 p-1 rounded-lg">
+          <div className="md:text-2xl flex gap-1 font-bold text-primary-foreground">
+            <div className="flex gap-1">
+              {timeLeft.days} <p>:</p>
+            </div>
+            <div className="flex gap-1">
+              {timeLeft.hours} <p>:</p>
+            </div>
+            <div className="flex gap-1">
+              {timeLeft.minutes} <p>:</p>
+            </div>
+            <div>{timeLeft.seconds} </div>
           </div>
         </div>
       </div>
