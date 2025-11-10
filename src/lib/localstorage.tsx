19dff81428a8f399be34e18926f15013f535be7f
Vipin Kumawat <96615928+AVVKavvk@users.noreply.github.com>
2025-11-10 16:57:11 +0530

    Default Ifinish (#24)

--- src/lib/localstorage.tsx
+++ src/lib/localstorage.tsx
@@ -63,10 +63,8 @@ export const getSourceConfig = (source: string) => {
     default:
       return {
         name: "Direct",
-        redirectUrl:
-          "https://www.burraa.com/activities/skf-goa-river-marathon-2025",
-        carbLoadingLink:
-          "https://www.burraa.com/activities/skf-goa-river-marathon-2025",
+        redirectUrl: "https://www.ifinish.in/running/SKF2025",
+        carbLoadingLink: "https://www.ifinish.in/entertainment/skfppa",
       };
   }
 };
